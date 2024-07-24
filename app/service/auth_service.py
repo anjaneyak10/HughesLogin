29e6ac999536978b1a3bd40c93cd82e44dc9f7be
Jayesh Pamnani <jayeshpamnani15@gmail.com>
2024-07-24 12:27:42 -0400

    changed hashing logic - using Bcrypt for hashing

--- app/service/auth_service.py
+++ app/service/auth_service.py
@@ -1,17 +1,18 @@
 from app.repository.user_repository import UserRepository
-# from werkzeug.security import generate_password_hash, check_password_hash
-from flask_bcrypt import generate_password_hash, check_password_hash
-
+from flask_bcrypt import Bcrypt
 import jwt
 import datetime
 from flask import current_app
+
+bcrypt = Bcrypt()
 
 class AuthService:
 
     @staticmethod
     def login(username, password):
         user = UserRepository.find_by_username(username)
-        if user and check_password_hash(user['password_hash'], password):
+        # print(check_password_hash(user['password_hash'], password))
+        if user and bcrypt.check_password_hash(user['password_hash'], password):
             token = AuthService.encode_auth_token(user)
             return {'token': token}
         return None
@@ -20,7 +21,7 @@ class AuthService:
     def register(email, name, username, role, function, password):
         if UserRepository.find_by_username(username):
             return None
-        password_hash = generate_password_hash(password)
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
         user_id = UserRepository.save(email, name, username, role, function, password_hash)
         return {'id': user_id, 'username': username}
 
